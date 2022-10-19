497eac669a04ddece19bb150e0432df2d0869587
JUNAID-PK <junaidhussnain369@gmail.com>
2022-10-19 19:26:57 +0500

    React Js app via create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [text, setText] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          Example textarea
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+          value={text}
+          onChange={(e)=>{
+            setText(e.target.value);
+          }}
+        ></textarea>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="btn btn-primary" onClick={converToUpperCase}>Convert To UpperCase</div>
+        <div className="btn btn-primary" onClick={removeSpaces}>Remove Spaces</div>
+        <div className="btn btn-primary" onClick={removeSymbols}>Remove Symbols</div>
+        <div className="btn btn-primary" onClick={removeSymbols}>Remove Symbols</div>
+        <div className="btn btn-primary" onClick={removeSymbols}>Remove Symbols</div>
+      </div>
+      </div>
     </div>
   );
+
+  // Functions
+  // Convert to UpperCase
+  function converToUpperCase(){
+    setText(text.toUpperCase());
+  }
+  // Remove Extra Spaces
+  function removeSpaces(){
+    setText(text.replace(/\s+/g, ' ').trim());
+  }
+  // Remove Symbols
+  function removeSymbols(){
+    setText(text.replace(/[^\w\s]/gi, ''));
+  }
 }
 
 export default App;
